81e041c003d5253f28ff552526ce27ca68bd0e12
Wout Sluiter <wsluiter@myonlinestore.com>
2018-04-06 14:36:03 +0200

    fixup! Added MOSTheme provider, moved types

--- config/storybook/decorators/themeDecorator.tsx
+++ config/storybook/decorators/themeDecorator.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MosTheme from '../../../src/themes/MosTheme/MosTheme.template';
+import MosTheme from '../../../src/themes/MosTheme';
 
 const themeDecorator = (story:Function):JSX.Element => (
     <MosTheme>{story()}</MosTheme>
