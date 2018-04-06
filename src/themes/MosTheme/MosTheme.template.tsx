b58d9dd221130d1bb297e7f5f25263657e2eae0a
Wout Sluiter <wsluiter@myonlinestore.com>
2018-04-06 16:56:41 +0200

    fixup! Added MOSTheme provider, moved types

--- src/themes/MosTheme/MosTheme.template.tsx
+++ src/themes/MosTheme/MosTheme.template.tsx
@@ -2,7 +2,7 @@ import React, { StatelessComponent } from 'react';
 import { ThemeProvider } from '../../utility/styled';
 import theme from './MosTheme.theme';
 
-const MosTheme:StatelessComponent<{}> = (props):JSX.Element => {
+const MosTheme:StatelessComponent = (props):JSX.Element => {
     return (
         <ThemeProvider theme={theme}>
             {props.children}
