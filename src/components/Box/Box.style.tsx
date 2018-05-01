a10a4561462904b652e26fa3a8ff961219467749
Wout Sluiter <wsluiter@myonlinestore.com>
2018-05-01 11:49:07 +0200

    fixup! Added Box component

--- src/components/Box/Box.style.tsx
+++ src/components/Box/Box.style.tsx
@@ -1,7 +1,7 @@
 import { StyledComponentClass as _S } from 'styled-components';
+import { OffsetType } from '../../types/OffsetType';
 import styled from '../../utility/styled';
 import Box from './Box.template';
-import formatOffset from './formatters/formatOffset';
 
 const StyledBox = styled(Box)`
     display: flex;
@@ -11,7 +11,21 @@ const StyledBox = styled(Box)`
     justify-content: ${({ justifyContent }):string => justifyContent !== undefined ? justifyContent : 'flex-start' };
     align-items: ${({ alignItems }):string => alignItems !== undefined ? alignItems : 'stretch' };
     align-content: ${({ alignContent }):string => alignContent !== undefined ? alignContent : 'stretch' };
-    margin: ${({ margin }):string => margin !== undefined ? formatOffset(margin) : '0'};
+    margin: ${({ margin }):string => {
+        if (margin !== undefined) {
+            const offsetValues = margin.map((value:OffsetType):string => {
+                if (value === 'auto') {
+                    return value;
+                }
+
+                return `${value}px`;
+            });
+
+            return offsetValues.join(' ');
+        }
+
+        return '0';
+    }};
     flex-grow: ${({ grow }):number => grow ? grow : 0 };
     flex-shrink: ${({ shrink }):number => shrink ? shrink : 1 };
     flex-basis: ${({ basis }):string => basis ? basis : 'auto' };
