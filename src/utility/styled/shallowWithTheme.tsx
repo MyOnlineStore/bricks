cbe66e827de3e5f5064fb01a6a9d2820d442427e
Wout Sluiter <wsluiter@myonlinestore.com>
2018-05-14 16:50:19 +0200

    fixup! Added ButtonGroup

--- src/utility/styled/shallowWithTheme.tsx
+++ src/utility/styled/shallowWithTheme.tsx
@@ -2,15 +2,15 @@ import { shallow, ShallowWrapper } from 'enzyme';
 import React from 'react';
 import MosTheme from '../../themes/MosTheme';
 
+/* tslint:disable */
 const shallowWithTheme = (component:JSX.Element):ShallowWrapper => {
-    /* tslint:disable */
     const context = shallow(<MosTheme />)
         .dive()
         .instance()
         .getChildContext();
-    /* tslint:enable */
 
     return shallow(component, { context });
 };
+/* tslint:enable */
 
 export default shallowWithTheme;
