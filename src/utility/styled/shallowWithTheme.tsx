7a4d534609debb56f3919e7ef3bf23ad32be2d4d
Wout Sluiter <wsluiter@myonlinestore.com>
2018-05-14 16:45:35 +0200

    fixup! Added styles to Text

--- src/utility/styled/shallowWithTheme.tsx
+++ src/utility/styled/shallowWithTheme.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import MosTheme from '../../themes/MosTheme';
 
 const shallowWithTheme = (component:JSX.Element):ShallowWrapper => {
+    /* tslint:disable */
     const context = shallow(<MosTheme />)
         .dive()
         .instance()
         .getChildContext();
+    /* tslint:enable */
 
     return shallow(component, { context });
 };
