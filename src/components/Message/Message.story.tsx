4f109f638ec94987d17096898ce23752e8e2bd01
Wout Sluiter <wsluiter@myonlinestore.com>
2018-05-15 23:21:52 +0200

    fixup! Added Message component

--- src/components/Message/Message.story.tsx
+++ src/components/Message/Message.story.tsx
@@ -1,41 +1,17 @@
-import { boolean } from '@storybook/addon-knobs/react';
+import { boolean, select, text } from '@storybook/addon-knobs/react';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import Message from '.';
 
 storiesOf('Message', module)
-    .add('Success', () => (
+    .add('Default', () => (
         <Message
-            severity="success"
-            title="Wow, Great job!"
-            message="This went very well."
-            date="A few minutes ago"
+            severity={select('severity', ['success', 'warning', 'error', 'info'], 'success')}
+            title={text('title', 'Title')}
+            message={text('message', 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus.')}
+            date={text('date', 'Today 12:00')}
             read={boolean('read', false)}
-        />
-    ))
-    .add('Error', () => (
-        <Message
-            severity="error"
-            title="Oops!"
-            message="Something went wrong. Please try again."
-            date="Yesterday 12:12"
-            read={boolean('read', false)}
-        />
-    ))
-    .add('Warning', () => (
-        <Message
-            severity="warning"
-            title="Careful!"
-            message="This might go wrong."
-            read={boolean('read', false)}
-        />
-    ))
-    .add('Info', () => (
-        <Message
-            severity="info"
-            title="FYI"
-            message="This just is happend."
-            date="12-12-2012"
-            read={boolean('read', false)}
+            buttonLabel={text('buttonLabel', 'Click me')}
+            action={():void => { alert('click'); }}
         />
     ));
--- src/components/Message/Message.template.tsx
+++ src/components/Message/Message.template.tsx
@@ -1,6 +1,9 @@
 import React, { StatelessComponent } from 'react';
 import { StyledType } from '../../utility/styled';
 import trbl from '../../utility/trbl';
+import Box from '../Box';
+import Button from '../Button';
+import { PropsType as ButtonPropsType } from '../Button/Button.template';
 import Spacer from '../Spacer';
 import Text from '../Text';
 
@@ -8,21 +11,45 @@ type PropsType = StyledType & {
     severity:'success'|'info'|'warning'|'error';
     title:string;
     message:string;
+    buttonLabel?:string;
     date?:string;
-    action?:Function;
     read?:boolean;
+    action?():void;
 };
 
 const Message:StatelessComponent<PropsType> = (props):JSX.Element => {
+    const mapVariant = (severity:PropsType['severity']):ButtonPropsType['variant'] => {
+        switch (severity) {
+            case 'warning':
+                return 'warning';
+            case 'error':
+                return 'destructive';
+            case 'info':
+                return 'secondary';
+            case 'success':
+            default:
+                return 'primary';
+        }
+    };
+
     return (
         <div className={props.className}>
             <Spacer
                 offset={trbl(12, 24)}
                 offsetType="inner"
             >
-                <Text strong>{props.title}</Text>
-                <Text>{props.message}</Text>
-                <Text descriptive>{props.date}</Text>
+                <Box wrap alignItems="center" alignContent="flex-start">
+                    <Box direction="column" grow={1} basis={'300px'} margin={trbl(0, 12, 0, 0)}>
+                        <Text strong>{props.title}</Text>
+                        <Text>{props.message}</Text>
+                        <Text descriptive>{props.date}</Text>
+                    </Box>
+                    {props.action !== undefined && props.buttonLabel !== undefined && props.buttonLabel.length > 0 &&
+                        <Box direction="column" basis="auto" justifyContent="center" alignItems="flex-end" margin={trbl(6, 0)}>
+                            <Button title={props.buttonLabel} variant={mapVariant(props.severity)} action={props.action}>{props.buttonLabel}</Button>
+                        </Box>
+                    }
+                </Box>
             </Spacer>
         </div>
     );
