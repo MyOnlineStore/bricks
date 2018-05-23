89ee03864629c6284e4d05ea7457a3882a1f39c1
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2018-05-23 11:35:22 +0200

    fixup! Updated button theme and removed FlatButton

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,4 @@ export { default as Spacer } from './components/Spacer';
 export { default as SubHeading } from './components/SubHeading';
 export { default as Text } from './components/Text';
 export { default as trbl } from './utility/trbl';
+export { default as Contrast } from './components/Contrast';
