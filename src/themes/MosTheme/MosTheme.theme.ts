13a4aee8f0e67763f6af90ab34e8728e3485cc1d
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2018-05-23 13:44:08 +0200

    fixup! Updated button theme and removed FlatButton

--- src/themes/MosTheme/MosTheme.theme.ts
+++ src/themes/MosTheme/MosTheme.theme.ts
@@ -64,21 +64,21 @@ const theme:ThemeType = {
                 backgroundColor: green.darker1,
                 borderColor: 'transparent',
                 color: silver.lighter1,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.2) inset',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .2) inset',
                 textDecoration: 'none',
             },
             hover: {
                 backgroundColor: green.darker2,
                 borderColor: 'transparent',
                 color: silver.lighter1,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.2) inset',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .2) inset',
                 textDecoration: 'none',
             },
             focus: {
                 backgroundColor: green.darker1,
                 borderColor: 'transparent',
                 color: silver.lighter1,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.2) inset, 0 0 0 4px rgba(107, 222, 120, 0.4)',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .2) inset, 0 0 0 4px rgba(107, 222, 120, .4)',
                 textDecoration: 'none',
             },
             active: {
@@ -94,21 +94,21 @@ const theme:ThemeType = {
                 backgroundColor: silver.base,
                 borderColor: 'transparent',
                 color: grey.base,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.2) inset',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .2) inset',
                 textDecoration: 'none',
             },
             hover: {
                 backgroundColor: silver.darker1,
                 borderColor: 'transparent',
                 color: grey.base,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.2) inset',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .2) inset',
                 textDecoration: 'none',
             },
             focus: {
                 backgroundColor: silver.base,
                 borderColor: 'transparent',
                 color: grey.base,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.2) inset, 0 0 0 4px rgba(51, 55, 64, 0.08)',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .2) inset, 0 0 0 4px rgba(51, 55, 64, .08)',
                 textDecoration: 'none',
             },
             active: {
@@ -124,21 +124,21 @@ const theme:ThemeType = {
                 backgroundColor: yellow.darker1,
                 borderColor: 'transparent',
                 color: silver.lighter1,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.2) inset',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .2) inset',
                 textDecoration: 'none',
             },
             hover: {
                 backgroundColor: yellow.darker2,
                 borderColor: 'transparent',
                 color: silver.lighter1,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.2) inset',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .2) inset',
                 textDecoration: 'none',
             },
             focus: {
                 backgroundColor: yellow.darker1,
                 borderColor: 'transparent',
                 color: silver.lighter1,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.1) inset, 0 0 0 3px rgba(252, 194, 0, 0.25)',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .1) inset, 0 0 0 3px rgba(252, 194, 0, .25)',
                 textDecoration: 'none',
             },
             active: {
@@ -154,21 +154,21 @@ const theme:ThemeType = {
                 backgroundColor: red.base,
                 borderColor: 'transparent',
                 color: silver.lighter1,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.2) inset',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .2) inset',
                 textDecoration: 'none',
             },
             hover: {
                 backgroundColor: red.darker1,
                 borderColor: 'transparent',
                 color: silver.lighter1,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.2) inset',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .2) inset',
                 textDecoration: 'none',
             },
             focus: {
                 backgroundColor: red.base,
                 borderColor: 'transparent',
                 color: silver.lighter1,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.1) inset, 0 0 0 3px rgba(237, 33, 87, 0.25)',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .1) inset, 0 0 0 3px rgba(237, 33, 87, .25)',
                 textDecoration: 'none',
             },
             active: {
@@ -191,14 +191,14 @@ const theme:ThemeType = {
                 backgroundColor: silver.base,
                 borderColor: 'transparent',
                 color: grey.base,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.2) inset',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .2) inset',
                 textDecoration: 'underline',
             },
             focus: {
                 backgroundColor: silver.base,
                 borderColor: 'transparent',
                 color: grey.base,
-                boxShadow: '0 -2px 0 rgba(0, 0, 0, 0.2) inset, 0 0 0 4px rgba(51, 55, 64, 0.08)',
+                boxShadow: '0 -2px 0 rgba(0, 0, 0, .2) inset, 0 0 0 4px rgba(51, 55, 64, .08)',
                 textDecoration: 'underline',
             },
             active: {
@@ -212,7 +212,7 @@ const theme:ThemeType = {
         disabled:{
             color: grey.lighter2,
             backgroundColor: silver.darker2,
-            stripingColor: 'rgba(0, 0, 0, 0.04)',
+            stripingColor: 'rgba(0, 0, 0, .04)',
         },
     },
     Contrast: {
@@ -233,7 +233,7 @@ const theme:ThemeType = {
                     },
                     focus: {
                         backgroundColor: silver.darker1,
-                        boxShadow: `0 -2px 0 rgba(0, 0, 0, 0.2) inset, 0 0 0 4px ${silver.darker2}`,
+                        boxShadow: `0 -2px 0 rgba(0, 0, 0, .2) inset, 0 0 0 4px ${silver.darker2}`,
                     },
                 },
                 flat: {
@@ -245,7 +245,7 @@ const theme:ThemeType = {
                     },
                     focus: {
                         backgroundColor: silver.darker1,
-                        boxShadow: `0 -2px 0 rgba(0, 0, 0, 0.2) inset, 0 0 0 4px ${silver.darker2}`,
+                        boxShadow: `0 -2px 0 rgba(0, 0, 0, .2) inset, 0 0 0 4px ${silver.darker2}`,
                     },
                 }
             }
