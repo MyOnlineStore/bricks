9ed8d24ffdb30c7c5d679c485bedb2f760ec8d03
Wout Sluiter <wsluiter@myonlinestore.com>
2018-06-12 17:47:37 +0200

    Added Badge

---
+++ src/components/Badge/index.tsx
@@ -0,0 +1,8 @@
+import React, { SFC } from 'react';
+import StyledBadge from './style';
+
+const Badge: SFC<{}> = (props): JSX.Element => {
+    return <StyledBadge>{props.children}</StyledBadge>;
+};
+
+export default Badge;
