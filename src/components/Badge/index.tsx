628f302d2a056217c7feddae86401af41c0effbb
Wout Sluiter <wsluiter@myonlinestore.com>
2018-06-13 08:49:38 +0200

    fixup! Added Badge

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,7 +1,7 @@
 import React, { SFC } from 'react';
 import StyledBadge from './style';
 
-const Badge: SFC<{}> = (props): JSX.Element => {
+const Badge: SFC = (props): JSX.Element => {
     return <StyledBadge>{props.children}</StyledBadge>;
 };
 
