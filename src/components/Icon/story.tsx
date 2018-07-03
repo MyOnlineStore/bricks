f02804f7dd5f08fa67efc61215009fef655f93af
Henk Borghols <henk@MacBook-Pro-2.mijnwebwinkel.local>
2018-07-03 16:27:04 +0200

    Added support for an additional 12x12 icon set

--- src/components/Icon/story.tsx
+++ src/components/Icon/story.tsx
@@ -5,16 +5,21 @@ import trbl from '../../utility/trbl';
 import Spacer from '../Spacer';
 
 storiesOf('Icon', module)
-    .add('Small icon', () => <Icon size="small" icon="heartO" />)
-    .add('Large icon', () => <Icon size="large" icon="heart" />)
+    .add('Small icon', () => <Icon size="small" icon="checkmark" />)
+    .add('Medium icon', () => <Icon size="medium" icon="heartO" />)
+    .add('Large icon', () => <Icon size="large" icon="mos" />)
     .add('With overwritten fill', () => <Icon color="BlueViolet" size="large" icon="heart" />)
     .add('With some text', () => (
         <div>
-            <Icon size="small" icon="mos" />
+            <Icon size="small" icon="checkmark" />
             <span> With some text</span>
             <Spacer offset={trbl(12, 0, 0, 0)}>
-                <Icon size="large" icon="award" />
+                <Icon size="medium" icon="lightbulb" />
                 <span> With some other text</span>
+                <Spacer offset={trbl(18, 0, 0, 0)}>
+                    <Icon size="large" icon="mos" />
+                    <span> With some other other text</span>
+                </Spacer>
             </Spacer>
         </div>
     ));
