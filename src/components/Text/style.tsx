4d3cd0153a964477e3e2df070e5789964dc52e04
Robin van der Voort <ro.vdv@hotmail.com>
2018-07-04 16:19:35 +0200

    Added needed imports

--- src/components/Text/style.tsx
+++ src/components/Text/style.tsx
@@ -1,3 +1,6 @@
+import _R from 'react';
+import { StyledComponentClass as _S } from 'styled-components';
+import _T from '../../types/ThemeType';
 import styled, { withProps } from '../../utility/styled';
 
 type TextVariantStyleType = {
@@ -29,11 +32,12 @@ type TextPropsType = {
     variant?: 'small' | 'base' | 'large';
 };
 
-const StyledText = withProps<TextPropsType>(styled.p)`
+const StyledText = withProps<TextPropsType, HTMLParagraphElement>(styled.p)`
     color: ${({ descriptive, theme }): string =>
         descriptive ? theme.Text.descriptive.color : theme.Text.default.color};
     font-family: ${({ theme }): string => theme.Text.default.fontFamily};
-    font-size: ${({ variant, theme }): string => variant !== undefined ? theme.Text[variant].fontSize : theme.Text.base.fontSize };
+    font-size: ${({ variant, theme }): string =>
+        variant !== undefined ? theme.Text[variant].fontSize : theme.Text.base.fontSize};
     font-weight: ${({ strong, theme }): string =>
         strong ? theme.Text.strong.fontWeight : theme.Text.default.fontWeight};
     line-height: ${({ compact }): string => (compact ? '1.25' : '1.5')};
