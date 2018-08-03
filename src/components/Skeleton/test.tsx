69782f54d40d3bdf9768d76aa11a89c954e14ba0
Nico Koelewijn <nico.koelewijn@live.com>
2018-08-03 13:31:12 +0200

    fixup! Added text skeleton

--- src/components/Skeleton/test.tsx
+++ src/components/Skeleton/test.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { mountWithTheme } from '../../utility/styled';
 import Skeleton from '.';
-<<<<<<< HEAD
 import StyledTextSkeleton from './Text/style';
-=======
->>>>>>> 65d0195... Added text skeleton
 
 describe('Skeleton', () => {
     it('should not crash when no baseWidth is set', () => {
@@ -14,13 +11,10 @@ describe('Skeleton', () => {
 
         expect(fn).not.toThrow();
     });
-<<<<<<< HEAD
 
     it('should not render placeholders less than 6px wide', () => {
         const component = mountWithTheme(<Skeleton.Text baseWidth={3} lines={1} />);
 
         expect(component.find(StyledTextSkeleton).prop('baseWidth')).toBe(6);
     });
-=======
->>>>>>> 65d0195... Added text skeleton
 });
--- src/utility/calculateRandomPercentage/index.ts
+++ src/utility/calculateRandomPercentage/index.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const calculateRandomPercentage = (min: number, max: number): number => {
-=======
-const calculateRandomPercentage = (min:number, max:number):number => {
->>>>>>> 65d0195... Added text skeleton
     const minBoundary = min < 0 ? 0 : min;
     const maxBoundary = max > 100 ? 100 : max;
 
