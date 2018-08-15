23632e364c7cf52837644710c6be49d032a6030c
Robin van der Voort <ro.vdv@hotmail.com>
2018-08-15 14:44:01 +0200

    added missing imports

--- src/components/Raised/style.tsx
+++ src/components/Raised/style.tsx
@@ -1,5 +1,6 @@
 import _R from 'react';
 import { StyledComponentClass as _S } from 'styled-components';
+import _T from '../../types/ThemeType';
 import styled, { withProps } from '../../utility/styled';
 
 type RaisedLevelThemeType = {
--- src/components/Tile/style.tsx
+++ src/components/Tile/style.tsx
@@ -1,5 +1,6 @@
 import _R from 'react';
 import { StyledComponentClass as _S } from 'styled-components';
+import _T from '../../types/ThemeType';
 import styled from '../../utility/styled';
 
 const StyledTile = styled.div`
