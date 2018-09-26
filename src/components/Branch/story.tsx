798e05efa879b012b153f8ff647de60616f5aace
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2018-09-26 14:54:04 +0200

    Added branch component

---
+++ src/components/Branch/story.tsx
@@ -0,0 +1,19 @@
+import { boolean } from '@storybook/addon-knobs/react';
+import Branch from '.';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import Text from '../Text';
+
+storiesOf('Branch', module).add('Default', () => {
+    return (
+        <Branch
+            condition={boolean('condition', true)}
+            ifTrue={(children): JSX.Element => <Text>This is shown when condition is true. {children}</Text>}
+            ifFalse={(children): JSX.Element => (
+                <Text>And this is will be shown when condition is false. {children}</Text>
+            )}
+        >
+            This line is shared wether the condition is true or not.
+        </Branch>
+    );
+});
