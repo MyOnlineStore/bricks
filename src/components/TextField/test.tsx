cbb421508c7b83647b942d4b1806f05e9167302d
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2018-09-28 10:33:42 +0200

    Added blut test

--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import TextField from '.';
-import { Icon } from '../../index';
 import { mountWithTheme } from '../../utility/styled/testing';
-import { StyledInput, StyledWrapper } from './style';
+import { StyledInput } from './style';
 
 describe('TextField', () => {
     it('should not change value when disabled', () => {
@@ -45,23 +44,5 @@ describe('TextField', () => {
         component.find(StyledInput).simulate('change');
 
         expect(changeMock).toHaveBeenCalled();
-    });
-
-    it('should not render a different icon and have straight corners when the feedback is of severity info', () => {
-        const component = mountWithTheme(
-            <TextField
-                value="John"
-                name="firstName"
-                onChange={(): void => undefined}
-                feedback={{
-                    severity: 'info',
-                    message: 'Hey, listen',
-                }}
-            />,
-        );
-        /* tslint:disable */
-        (expect(component.find(StyledWrapper)).not as any).toHaveStyleRule('border-bottom-right-radius');
-        /* tslint:enable */
-        expect(component.find(Icon).prop('icon')).toEqual('questionCircle');
     });
 });
