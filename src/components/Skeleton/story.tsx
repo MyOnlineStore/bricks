5f199d6e23513e3850b146a5770af171afa13810
Nico Koelewijn <nicokoelewijn@users.noreply.github.com>
2018-10-30 16:29:33 +0100

    Removed Skeleton button (#238)

--- src/components/Skeleton/story.tsx
+++ src/components/Skeleton/story.tsx
@@ -3,7 +3,6 @@ import React, { SFC } from 'react';
 import Skeleton from '.';
 import { boolean, number } from '@storybook/addon-knobs';
 import Text from '../Text';
-import Button from '../Button';
 
 type DemoPropsType = {
     loading: boolean;
@@ -11,26 +10,17 @@ type DemoPropsType = {
 };
 
 const Demo: SFC<DemoPropsType> = ({ element, loading }): JSX.Element => {
-    if (element === 'Text') {
-        if (loading) {
-            return <Skeleton.Text lines={number('lines', 3)} baseWidth={number('baseWidth', 80)} />;
-        }
-
-        return (
-            <>
-                <Text>Leverage agile frameworks to provide a robust synopsis for high level overviews.</Text>
-                <Text>Iterative approaches to corporate strategy foster collaborative</Text>
-                <Text>thinking to further the overall value proposition.</Text>
-            </>
-        );
-    } else {
-        if (loading) {
-            return <Skeleton.Button width={number('width', 139)} />;
-        }
-
-        return <Button title="The button has loaded" variant="primary" />;
+    if (loading) {
+        return <Skeleton.Text lines={number('lines', 3)} baseWidth={number('baseWidth', 80)} />;
     }
+
+    return (
+        <>
+            <Text>Leverage agile frameworks to provide a robust synopsis for high level overviews.</Text>
+            <Text>Iterative approaches to corporate strategy foster collaborative</Text>
+            <Text>thinking to further the overall value proposition.</Text>
+        </>
+    );
 };
 
 storiesOf('Skeleton', module).add('Text', () => <Demo element="Text" loading={boolean('loading', true)} />);
-storiesOf('Skeleton', module).add('Button', () => <Demo element="Button" loading={boolean('loading', true)} />);
