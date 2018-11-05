18eb4f6d4f74b0648eadf471abeabbd54226713e
hborghols <henk@myonlinestore.com>
2018-11-05 10:26:33 +0100

    fixup! changed hexToRGB to rgba converter

--- src/utility/rgba/index.ts
+++ src/utility/rgba/index.ts
@@ -1,5 +1,20 @@
 type ArgumentsType = [string] | [string, number] | [number, number, number] | [number, number, number, number];
 
+export const hexToRGB = (hex: string): string => {
+    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex
+        .replace(/[^0-9A-F]/gi, '')
+        .replace(shorthandRegex, (m: string, r: string, g: string, b: string): string => {
+            return r + r + g + g + b + b;
+        });
+    const bigint = parseInt(hex, 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+
+    return `${r},${g},${b}`;
+};
+
 const rgba = (...args: ArgumentsType): string => {
     switch (args.length) {
         case 1: // hex to rgba with opacity = 1
@@ -8,21 +23,10 @@ const rgba = (...args: ArgumentsType): string => {
             return `rgba(${hexToRGB(args[0])},${args[1]})`;
         case 3: // rgb to rgba with opacity 1
             return `rgba(${args[0]},${args[1]},${args[2]},1)`;
-        case 4: // rgb to rgba with opacity = args[3]
+        default:
+            // rgb to rgba with opacity = args[3]
             return `rgba(${args[0]},${args[1]},${args[2]},${args[3]})`;
     }
-};
-
-export const hexToRGB = (hex: string): string => {
-    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(/[^0-9A-F]/gi, '').replace(shorthandRegex, function(m, r, g, b) {
-        return r + r + g + g + b + b;
-    });
-    const bigint = parseInt(hex, 16);
-    const r = (bigint >> 16) & 255;
-    const g = (bigint >> 8) & 255;
-    const b = bigint & 255;
-    return `${r},${g},${b}`;
 };
 
 export default rgba;
