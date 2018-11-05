4be3ea192bfa9e4b9cf39a812601dbaa323898b4
hborghols <henk@myonlinestore.com>
2018-11-05 13:33:56 +0100

    fixup! Removed debug line in Dockerfile

--- src/utility/SubscriptionContext/index.tsx
+++ src/utility/SubscriptionContext/index.tsx
@@ -20,7 +20,7 @@ type ContextValue = {
     getPayload: SubscriptionProvider['getPayload'];
 };
 
-const { Consumer, Provider } = createContext(undefined as any);
+const { Consumer, Provider } = createContext(undefined as ContextValue);
 
 const SubscriptionConsumer: Consumer<ContextValue> = Consumer;
 
