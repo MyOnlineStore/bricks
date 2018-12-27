48d42b98e29e11304485cd7e49e7ea50186821cc
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2018-12-27 07:54:39 +0100

    Removed types in component folders

--- src/components/Skeleton/Rect/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { FunctionComponent } from 'react';
-declare type PropsType = {
-    width?: string;
-    height?: string;
-};
-declare const Rect: FunctionComponent<PropsType>;
-export default Rect;
-export { PropsType };
