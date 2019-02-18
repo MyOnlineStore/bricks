de8c5049dbb7e89422c7f7f7644888b20af87f0b
Wout Sluiter <wsluiter@myonlinestore.com>
2019-02-18 13:55:57 +0100

    Added MosTheme fallback for MerchantTheme

--- src/themes/MerchantTheme/index.tsx
+++ src/themes/MerchantTheme/index.tsx
@@ -1,13 +1,19 @@
 import React, { FunctionComponent } from 'react';
 import ThemeType from '../../types/ThemeType';
 import { ThemeProvider } from '../../utility/styled';
+import { default as MosTheme } from '../MosTheme/MosTheme.theme';
+import deepmerge from 'deepmerge';
 
 type PropsType = {
     theme: ThemeType;
 };
 
+const customTheme = (theme: ThemeType): ThemeType => {
+    return deepmerge(MosTheme, theme);
+};
+
 const MerchantTheme: FunctionComponent<PropsType> = (props): JSX.Element => (
-    <ThemeProvider theme={props.theme}>{props.children}</ThemeProvider>
+    <ThemeProvider theme={customTheme(props.theme)}>{props.children}</ThemeProvider>
 );
 
 export default MerchantTheme;
