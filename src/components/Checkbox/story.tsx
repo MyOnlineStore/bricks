82a3f529115e10d178ef3eb7bb8f3ce4c40ad6e9
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2019-02-21 14:46:26 +0100

    Made story checkable

--- src/components/Checkbox/story.tsx
+++ src/components/Checkbox/story.tsx
@@ -1,16 +1,33 @@
 import { boolean, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
-import React from 'react';
+import React, { Component } from 'react';
 import Checkbox from '.';
 
-storiesOf('Checkbox', module).add('Default', () => (
-    <Checkbox
-        onChange={(): void => undefined}
-        value="bar"
-        checked={boolean('checked', true)}
-        disabled={boolean('disabled', false)}
-        error={boolean('error', false)}
-        name="foo"
-        label={text('label', undefined)}
-    />
-));
+type StateType = { checked: boolean };
+type PropsType = {};
+
+class Demo extends Component<PropsType, StateType> {
+    public constructor(props: PropsType) {
+        super(props);
+
+        this.state = {
+            checked: false,
+        };
+    }
+
+    public render(): JSX.Element {
+        return (
+            <Checkbox
+                onChange={({ checked }): void => this.setState({ checked: checked as boolean })}
+                value="bar"
+                checked={this.state.checked}
+                disabled={boolean('disabled', false)}
+                error={boolean('error', false)}
+                name="foo"
+                label={text('label', undefined)}
+            />
+        );
+    }
+}
+
+storiesOf('Checkbox', module).add('Default', () => <Demo />);
