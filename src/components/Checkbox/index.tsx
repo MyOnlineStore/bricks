b29cfd0006b973be6b256ceac03632472c1dea93
Carlo Palinckx <carlopalinckx@gmail.com>
2019-02-21 14:48:49 +0100

    Checkbox label (#339)

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -2,6 +2,7 @@ import React, { Component, MouseEvent } from 'react';
 import Icon from '../Icon';
 import { StyledCheckbox, StyledCheckboxSkin } from './style';
 import Box from '../Box';
+import Text from '../Text';
 
 type StateType = {
     focus: boolean;
@@ -13,6 +14,7 @@ type PropsType = {
     error?: boolean;
     value: string;
     name: string;
+    label?: string;
     id?: string;
     onChange(change: { checked: boolean | 'indeterminate'; event: MouseEvent<HTMLDivElement> }): void;
 };
@@ -40,28 +42,38 @@ class Checkbox extends Component<PropsType, StateType> {
         const htmlChecked = this.props.checked === true;
 
         return (
-            <StyledCheckboxSkin
-                checkedState={this.props.checked}
-                onClick={(event): void => this.changeHandler(event)}
-                elementFocus={this.state.focus}
-                disabled={this.props.disabled}
-                error={this.props.error}
-            >
-                <Box justifyContent="center" alignItems="center" height="100%">
-                    {this.props.checked === true && <Icon size="small" color="#fff" icon="checkmark" />}
-                    {this.props.checked === 'indeterminate' && <Icon size="small" color="#fff" icon="minus" />}
+            <Box onClick={this.changeHandler}>
+                <Box shrink={0}>
+                    <StyledCheckboxSkin
+                        checkedState={this.props.checked}
+                        elementFocus={this.state.focus}
+                        disabled={this.props.disabled}
+                        error={this.props.error}
+                    >
+                        <Box justifyContent="center" alignItems="center" height="100%">
+                            {this.props.checked === true && <Icon size="small" color="#fff" icon="checkmark" />}
+                            {this.props.checked === 'indeterminate' && <Icon size="small" color="#fff" icon="minus" />}
+                        </Box>
+                        <StyledCheckbox
+                            onFocus={this.toggleFocus}
+                            onBlur={this.toggleFocus}
+                            readOnly
+                            name={this.props.name}
+                            value={this.props.value}
+                            id={this.props.id}
+                            checked={htmlChecked}
+                            type="checkbox"
+                        />
+                    </StyledCheckboxSkin>
                 </Box>
-                <StyledCheckbox
-                    onFocus={this.toggleFocus}
-                    onBlur={this.toggleFocus}
-                    readOnly
-                    name={this.props.name}
-                    value={this.props.value}
-                    id={this.props.id}
-                    checked={htmlChecked}
-                    type="checkbox"
-                />
-            </StyledCheckboxSkin>
+                {this.props.label !== undefined && (
+                    <Box margin={[-3, 0, 0, 12]}>
+                        <Text>
+                            <label htmlFor={this.props.name}>{this.props.label}</label>
+                        </Text>
+                    </Box>
+                )}
+            </Box>
         );
     }
 }
