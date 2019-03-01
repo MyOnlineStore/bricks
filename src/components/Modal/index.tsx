901ef8785110af5d0c9436b542de6dc8b61afc3a
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2019-03-01 08:55:26 +0100

    Integrated react-measure

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,7 +1,6 @@
 import React, { Component, ReactNode, RefObject, createRef } from 'react';
 import trbl from '../../utility/_trbl';
 import Box from '../Box';
-import BreakpointProvider from '../BreakpointProvider';
 import IconButton from '../IconButton';
 import Contrast from '../Contrast';
 import Heading from '../Heading';
@@ -10,6 +9,7 @@ import TransitionAnimation from '../TransitionAnimation';
 import StyledModal, { StyledModalWrapper } from './style';
 import ButtonGroup from '../ButtonGroup';
 import close from '../../assets/icons/close.svg';
+import Measure from 'react-measure';
 
 type PropsType = {
     show: boolean;
@@ -21,23 +21,22 @@ type PropsType = {
 };
 
 class Modal extends Component<PropsType> {
-    private styledModalRef: RefObject<HTMLDivElement>;
+    private styledModalRef: HTMLDivElement | null;
     private styledModalWrapperRef: RefObject<HTMLDivElement>;
 
     public constructor(props: PropsType) {
         super(props);
-        this.styledModalRef = createRef();
         this.styledModalWrapperRef = createRef();
     }
 
     public handleClickOutside = (event: Event): void => {
         if (
             this.styledModalWrapperRef.current !== null &&
-            this.styledModalRef.current !== null &&
+            this.styledModalRef !== null &&
             this.props.onClose !== undefined &&
             this.props.show &&
             this.styledModalWrapperRef.current.contains(event.target as Node) &&
-            !this.styledModalRef.current.contains(event.target as Node)
+            !this.styledModalRef.contains(event.target as Node)
         ) {
             this.props.onClose();
         }
@@ -63,65 +62,74 @@ class Modal extends Component<PropsType> {
         return (
             <StyledModalWrapper show={this.props.show} ref={this.styledModalWrapperRef}>
                 <TransitionAnimation key={0} show={this.props.show} animation="zoom">
-                    <BreakpointProvider breakpoints={{ small: 0, medium: 320, large: 1200 }}>
-                        {(breakpoint): JSX.Element => (
-                            <StyledModal
-                                modalSize={this.props.size !== undefined ? this.props.size : 'large'}
-                                ref={this.styledModalRef}
-                                role="dialog"
-                                aria-modal={true}
-                                aria-label={this.props.title}
-                            >
-                                <Box
-                                    shrink={0}
-                                    margin={breakpoint === 'small' ? trbl(18) : trbl(36)}
-                                    alignItems="flex-start"
-                                    alignContent="center"
-                                    justifyContent="space-between"
+                    <Measure bounds>
+                        {({ measureRef, contentRect }) => {
+                            const breakpoint = contentRect.bounds && contentRect.bounds.width < 320 ? 'small' : 'large';
+
+                            return (
+                                <StyledModal
+                                    modalSize={this.props.size !== undefined ? this.props.size : 'large'}
+                                    ref={node => {
+                                        this.styledModalRef = node;
+                                        measureRef(node);
+                                    }}
+                                    role="dialog"
+                                    aria-modal={true}
+                                    aria-label={this.props.title}
                                 >
-                                    <Heading hierarchy={2}>{this.props.title}</Heading>
-                                    {this.props.onClose !== undefined && (
-                                        <Box
-                                            margin={trbl(-12, -12, -6, 0)}
-                                            alignContent="center"
-                                            justifyContent="flex-end"
-                                            alignItems="center"
-                                            grow={0}
-                                        >
-                                            <IconButton
-                                                icon={close}
-                                                variant="primary"
-                                                title="close"
-                                                onClick={this.props.onClose}
-                                            />
-                                        </Box>
-                                    )}
-                                </Box>
-                                <ScrollBox>
-                                    <Box padding={breakpoint === 'small' ? trbl(0, 18, 18, 18) : trbl(0, 36, 36, 36)}>
-                                        {this.props.children}
+                                    <Box
+                                        shrink={0}
+                                        margin={breakpoint === 'small' ? trbl(18) : trbl(36)}
+                                        alignItems="flex-start"
+                                        alignContent="center"
+                                        justifyContent="space-between"
+                                    >
+                                        <Heading hierarchy={2}>{this.props.title}</Heading>
+                                        {this.props.onClose !== undefined && (
+                                            <Box
+                                                margin={trbl(-12, -12, -6, 0)}
+                                                alignContent="center"
+                                                justifyContent="flex-end"
+                                                alignItems="center"
+                                                grow={0}
+                                            >
+                                                <IconButton
+                                                    icon={close}
+                                                    variant="primary"
+                                                    title="close"
+                                                    onClick={this.props.onClose}
+                                                />
+                                            </Box>
+                                        )}
                                     </Box>
-                                </ScrollBox>
-                                {(this.props.renderFixed || this.props.buttons) && (
-                                    <Contrast>
+                                    <ScrollBox>
                                         <Box
-                                            direction="column"
-                                            alignItems="stretch"
-                                            shrink={0}
-                                            padding={breakpoint === 'small' ? trbl(24) : trbl(24, 36)}
+                                            padding={breakpoint === 'small' ? trbl(0, 18, 18, 18) : trbl(0, 36, 36, 36)}
                                         >
-                                            {this.props.renderFixed && this.props.renderFixed()}
-                                            {this.props.buttons && (
-                                                <ButtonGroup stacked={breakpoint === 'small'}>
-                                                    {this.props.buttons}
-                                                </ButtonGroup>
-                                            )}
+                                            {this.props.children}
                                         </Box>
-                                    </Contrast>
-                                )}
-                            </StyledModal>
-                        )}
-                    </BreakpointProvider>
+                                    </ScrollBox>
+                                    {(this.props.renderFixed || this.props.buttons) && (
+                                        <Contrast>
+                                            <Box
+                                                direction="column"
+                                                alignItems="stretch"
+                                                shrink={0}
+                                                padding={breakpoint === 'small' ? trbl(24) : trbl(24, 36)}
+                                            >
+                                                {this.props.renderFixed && this.props.renderFixed()}
+                                                {this.props.buttons && (
+                                                    <ButtonGroup stacked={breakpoint === 'small'}>
+                                                        {this.props.buttons}
+                                                    </ButtonGroup>
+                                                )}
+                                            </Box>
+                                        </Contrast>
+                                    )}
+                                </StyledModal>
+                            );
+                        }}
+                    </Measure>
                 </TransitionAnimation>
             </StyledModalWrapper>
         );
