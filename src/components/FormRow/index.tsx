fb940465378e915b76d636a06b4a95cd54c0328d
Johan van Tongeren <johan@mijnwebwinkel.nl>
2019-03-25 13:54:53 +0100

    Removed new functionality for vertical form rows

--- src/components/FormRow/index.tsx
+++ src/components/FormRow/index.tsx
@@ -1,34 +1,20 @@
 import React, { ReactNode, FunctionComponent } from 'react';
 import Box from '../Box';
+import trbl from '../../utility/_trbl';
 import { StyledFormRow } from './style';
 
 type PropsType = {
     label: ReactNode;
     field: ReactNode;
-    vertical?: boolean;
 };
 
 const FormRow: FunctionComponent<PropsType> = (props): JSX.Element => {
     return (
         <StyledFormRow>
-            <Box
-                basis={props.vertical ? '100%' : '180px'}
-                grow={1}
-                maxWidth={props.vertical ? '' : '241px'}
-                margin={[18, 9, 0, 0]}
-                justifyContent="stretch"
-                wrap
-            >
+            <Box basis={'180px'} grow={1} maxWidth="241px" margin={trbl(18, 9, 0, 0)} justifyContent="stretch" wrap>
                 {props.label}
             </Box>
-            <Box
-                basis={props.vertical ? '100%' : '180px'}
-                grow={1}
-                maxWidth={props.vertical ? '' : '470px'}
-                margin={[9, 0]}
-                alignItems="flex-start"
-                wrap
-            >
+            <Box basis={'180px'} grow={1} maxWidth="470px" margin={trbl(9, 0)} alignItems="flex-start" wrap>
                 {props.field}
             </Box>
         </StyledFormRow>
