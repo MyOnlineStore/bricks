a7ac89812c2e8f5021c547233011014ff56beaa5
Nico Koelewijn <nico.koelewijn@live.com>
2019-04-01 16:53:39 +0200

    updated textfield color in theme

--- src/themes/MosTheme/MosTheme.theme.ts
+++ src/themes/MosTheme/MosTheme.theme.ts
@@ -802,7 +802,7 @@ const theme: ThemeType = {
                 borderColor: silver.darker4,
                 fontSize: fontSize.base,
                 fontFamily: bodyFont,
-                color: grey.lighter3,
+                color: grey.base,
                 background: silver.lighter1,
             },
             placeholder: {
