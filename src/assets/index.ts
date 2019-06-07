ff957dcf6e47485b2bcb77bf211ce46f257c8fb1
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2019-06-07 11:39:47 +0200

    Added correct icon names (#394)

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -285,9 +285,9 @@ export { default as FormatIconsListNumber } from './icons/format-icons-list-numb
 export { default as FormatIconsVerticalCenter } from './icons/format-icons-vertical-center.svg';
 export { default as FormatIconsVerticalDown } from './icons/format-icons-vertical-down.svg';
 export { default as FormatIconsVerticalUp } from './icons/format-icons-vertical-top.svg';
-export { default as FormatFontsizeSmall } from './icons/format-fontsize-small.svg';
-export { default as FormatFontsizeMedium } from './icons/format-fontsize-medium.svg';
-export { default as FormatFontsizeLarge } from './icons/format-fontsize-large.svg';
+export { default as FormatFontsizeSmall } from './icons/format-fontsize-s.svg';
+export { default as FormatFontsizeMedium } from './icons/format-fontsize-m.svg';
+export { default as FormatFontsizeLarge } from './icons/format-fontsize-l.svg';
 export { default as FormatLetterCase } from './icons/format-letter-case.svg';
 export { default as FormatLetterLowercase } from './icons/format-letter-lowercase.svg';
 export { default as FormatLetterUppercase } from './icons/format-letter-uppercase.svg';
