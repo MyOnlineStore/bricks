70341833fe7b65571437e2ab9a85eacde65ab834
Nico Koelewijn <nicokoelewijn@users.noreply.github.com>
2019-07-16 13:46:09 +0200

    Additional Slider props (#404)

--- src/components/Slider/test.tsx
+++ src/components/Slider/test.tsx
@@ -41,10 +41,70 @@ describe('Slider', () => {
 
     it('should not crash when no onChange is provided', () => {
         const fn = (): void => {
-            mountWithTheme(<Slider value={5} minLimit={2} maxLimit={7} onChange={() => undefined} />);
+            const component = mountWithTheme(<Slider value={5} minLimit={2} maxLimit={7} onChange={undefined} />);
+            component
+                .find('input')
+                .first()
+                .simulate('change', { target: { value: '2' } });
         };
 
         expect(fn).not.toThrow();
+    });
+
+    it('should not crash when no onFocus is provided', () => {
+        const fn = (): void => {
+            const component = mountWithTheme(<Slider value={5} minLimit={2} maxLimit={7} onFocus={undefined} />);
+
+            component
+                .find('input')
+                .first()
+                .simulate('focus');
+        };
+
+        expect(fn).not.toThrow();
+    });
+
+    it('should not crash when no onBlur is provided', () => {
+        const fn = (): void => {
+            const component = mountWithTheme(<Slider value={5} minLimit={2} maxLimit={7} onBlur={undefined} />);
+
+            component
+                .find('input')
+                .first()
+                .simulate('focus')
+                .simulate('blur');
+        };
+
+        expect(fn).not.toThrow();
+    });
+
+    it('should be able to call onBlur when provided', () => {
+        const blurMock = jest.fn();
+        const component = mountWithTheme(<Slider value={5} minLimit={2} maxLimit={7} onBlur={blurMock} />);
+
+        expect(blurMock).not.toHaveBeenCalled();
+
+        component
+            .find('input')
+            .first()
+            .simulate('focus')
+            .simulate('blur');
+
+        expect(blurMock).toHaveBeenCalled();
+    });
+
+    it('should be able to call onFocus when provided', () => {
+        const focusMock = jest.fn();
+        const component = mountWithTheme(<Slider value={5} minLimit={2} maxLimit={7} onFocus={focusMock} />);
+
+        expect(focusMock).not.toHaveBeenCalled();
+
+        component
+            .find('input')
+            .first()
+            .simulate('focus');
+
+        expect(focusMock).toHaveBeenCalled();
     });
 
     it('should floor and ceil the minLimit and maxLimit respectively', () => {
@@ -54,5 +114,11 @@ describe('Slider', () => {
 
         expect(slider.props().minValue).toBe(1);
         expect(slider.props().maxValue).toBe(16);
+    });
+
+    it('should render the aria-label when provided', () => {
+        const component = mountWithTheme(<Slider label="foo-label" value={5} minLimit={1.75} maxLimit={15.13} />);
+
+        expect(component.find(InputSlider).prop('aria-label')).toBe('foo-label');
     });
 });
