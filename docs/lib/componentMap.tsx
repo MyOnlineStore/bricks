3b6574c207dfb5cb90e61c384a213233706e2b9b
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2019-07-24 09:46:33 +0200

    Removed any from componentMap

--- docs/lib/componentMap.tsx
+++ docs/lib/componentMap.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Heading, Text } from '@myonlinestore/bricks';
 
-const componentMap = {
-    h1: (props: any) => <Heading hierarchy={1}>{props.children}</Heading>,
-    h2: (props: any) => <Heading hierarchy={2}>{props.children}</Heading>,
-    h3: (props: any) => <Heading hierarchy={3}>{props.children}</Heading>,
-    h4: (props: any) => <Heading hierarchy={4}>{props.children}</Heading>,
-    h5: (props: any) => <Heading hierarchy={5}>{props.children}</Heading>,
-    h6: (props: any) => <Heading hierarchy={6}>{props.children}</Heading>,
-    p: (props: any) => <Text>{props.children}</Text>,
+const componentMap: { [key: string]: FC } = {
+    h1: props => <Heading hierarchy={1}>{props.children}</Heading>,
+    h2: props => <Heading hierarchy={2}>{props.children}</Heading>,
+    h3: props => <Heading hierarchy={3}>{props.children}</Heading>,
+    h4: props => <Heading hierarchy={4}>{props.children}</Heading>,
+    h5: props => <Heading hierarchy={5}>{props.children}</Heading>,
+    h6: props => <Heading hierarchy={6}>{props.children}</Heading>,
+    p: props => <Text>{props.children}</Text>,
 };
 
 export default componentMap;
