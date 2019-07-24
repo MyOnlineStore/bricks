01697ca91f1d2650b5699548c61d3d76857837c6
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2019-07-24 09:43:00 +0200

    Added doc generation to docs website

---
+++ docs/pages/components.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+import { Box } from '@myonlinestore/bricks';
+import Document from '@myonlinestore/bricks/components/Badge/readme.mdx';
+import componentMap from '../lib/componentMap';
+
+type PropsType = {};
+
+const Components: FC<PropsType> = props => {
+    return (
+        <Box>
+            <Document components={componentMap} />
+        </Box>
+    );
+};
+
+export default Components;
