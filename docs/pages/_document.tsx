170224338737ff21ce48a2c10176f8578fd13972
Carlo Palinckx <carlopalinckx@gmail.com>
2019-08-02 10:55:24 +0200

    Added readme-template for components (#417)

---
+++ docs/pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+import { ComponentType } from 'react';
+
+export default class AppDocument extends Document {
+    static async getInitialProps(ctx: any) {
+        const sheet = new ServerStyleSheet();
+        const originalRenderPage = ctx.renderPage;
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App: ComponentType<Object>) => (props: Object) =>
+                        sheet.collectStyles(<App {...props} />),
+                });
+
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
+    }
+}
