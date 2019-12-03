801bef040e708a758b43cd89f061c3f7a3d26c91
hborghols <henk@myonlinestore.com>
2019-12-03 15:47:39 +0100

    Remove unused import

--- src/components/TextArea/test.tsx
+++ src/components/TextArea/test.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from 'react';
+import React from 'react';
 import TextArea from '.';
 import { mountWithTheme } from '../../utility/styled/testing';
 import { StyledTextArea } from './style';
