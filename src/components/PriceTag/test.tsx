e4542cfed9fee0c6eb88b230229b2da9d8d6f9e0
hborghols <henk@myonlinestore.com>
2020-02-05 16:04:31 +0100

    fixup! fixup! fixup! #433 - Use font colour for strikethrough PriceTag

--- src/components/PriceTag/test.tsx
+++ src/components/PriceTag/test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PriceTag from '.';
 import { mountWithTheme } from '../../utility/styled/testing';
-import { mosTheme } from '../../themes/MosTheme';
 import 'jest-styled-components';
 
 const currencysymbols = {
