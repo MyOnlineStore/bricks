71e2d97653c24d0f6836d022ef258424247f67f0
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2020-02-14 13:20:26 +0100

    Adjust setup

--- jest.framework.js
+++ jest.framework.js
@@ -1,6 +1,6 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import 'jest-styled-components';
+const { configure } = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+require('jest-styled-components');
 
 configure({ adapter: new Adapter() });
 
