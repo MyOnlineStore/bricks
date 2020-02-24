a45c16b00c3cf48e707d09baa65357658a4e4c6d
Carlo Palinckx <carlopalinckx@gmail.com>
2020-02-24 11:40:12 +0100

    Turned repo into monorepo and added @myonlinestore/bricks-assets (#500)
    * Rollup for bricks
    
    * Moved to monorepo
    
    * Moved components and assets
    
    * Added gh action
    
    * Removed circle config in favor of actions

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import path from 'path';
+import { readdirSync } from 'fs';
+import visualizer from 'rollup-plugin-visualizer';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import peerDepsExternals from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx', '.svg'];
+const packageName = path.basename(process.cwd());
+
+const config = [
+    {
+        input: path.join(process.cwd(), 'src', 'index.ts'),
+        output: {
+            dir: 'dist',
+            format: 'umd',
+            exports: 'named',
+            name: `bricks-${packageName}`,
+        },
+        plugins: [
+            babel({
+                extensions,
+                exclude: 'node_modules/**',
+            }),
+            resolve({
+                extensions,
+            }),
+            commonjs(),
+            peerDepsExternals({
+                packageJsonPath: path.join(process.cwd(), 'package.json'),
+            }),
+            terser({
+                output: {
+                    comments: false,
+                },
+            }),
+            visualizer({
+                filename: 'reports/stats.html',
+            }),
+        ],
+    },
+];
+
+export default config;
