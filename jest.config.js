79b1583bcf7db42a405732e03441dee92e1357fc
Carlo Palinckx <carlopalinckx@gmail.com>
2020-02-24 11:40:12 +0100

    Turned repo into monorepo and added @myonlinestore/bricks-assets (#500)
    * Rollup for bricks
    
    * Moved to monorepo
    
    * Moved components and assets
    
    * Added gh action
    
    * Removed circle config in favor of actions

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,7 @@
 module.exports = {
     globals: {
         'ts-jest': {
-            babelConfig: {
-                plugins: [
-                    [
-                        'babel-plugin-styled-components',
-                        {
-                            ssr: true,
-                        },
-                    ],
-                ],
-            },
+            babelConfig: true,
         },
     },
     transform: {
@@ -42,8 +33,8 @@ module.exports = {
         '!src/utility/styled/*',
         '!src/utility/SubscriptionContext/index.tsx',
     ],
-    setupFilesAfterEnv: ['./jest.framework.tsx'],
-    setupFiles: ['./jest.setup.tsx'],
+    setupFilesAfterEnv: ['./jest.framework.js'],
+    setupFiles: ['./jest.setup.js'],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
             '<rootDir>/__mocks__/fileMock.js',
