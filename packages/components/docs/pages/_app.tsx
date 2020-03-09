7e4e3c6dc51558be4e828c9912fe82199784d796
Henk Borghols <henk@myonlinestore.com>
2020-03-09 13:41:55 +0100

    Move GlobalStyles from _document to _app (#516)

--- packages/components/docs/pages/_app.tsx
+++ packages/components/docs/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import App from 'next/app';
-import { Box, MosTheme } from '@myonlinestore/bricks';
+import { createGlobalStyle } from 'styled-components';
+import { MosTheme } from '@myonlinestore/bricks';
 import Head from '../components/Head';
 import Page from '../components/Page';
 
@@ -11,17 +12,38 @@ declare module 'react' {
     }
 }
 
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: Melbourne;
+    src: url('/static/fonts/melbourne-bold.ttf');
+    font-weight: 700;
+  }
+  @font-face {
+      font-family: Melbourne;
+      src: url('/static/fonts/melbourne.ttf');
+      font-weight: 400;
+  }
+  @font-face {
+      font-family: Melbourne;
+      src: url('/static/fonts/melbourne-light.ttf');
+      font-weight: 300;
+  }
+`;
+
 class MyApp extends App {
     public render() {
         const { Component, pageProps } = this.props;
 
         return (
-            <MosTheme>
-                <Head />
-                <Page>
-                    <Component {...pageProps} />
-                </Page>
-            </MosTheme>
+            <>
+                <GlobalStyle />
+                <MosTheme>
+                    <Head />
+                    <Page>
+                        <Component {...pageProps} />
+                    </Page>
+                </MosTheme>
+            </>
         );
     }
 }
