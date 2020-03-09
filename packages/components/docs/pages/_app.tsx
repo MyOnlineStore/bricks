cc2836a3963bce0a58aeb1023068ecc6294ae64f
hborghols <henk@myonlinestore.com>
2020-03-09 10:47:09 +0100

    fixup! Replace createGlobalStylesheet with Next global style

--- packages/components/docs/pages/_app.tsx
+++ packages/components/docs/pages/_app.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import App from 'next/app';
+import { createGlobalStyle } from 'styled-components';
 import { MosTheme } from '@myonlinestore/bricks';
 import Head from '../components/Head';
 import Page from '../components/Page';
-import '../styles.css';
 
 declare module 'react' {
     interface StyleHTMLAttributes<T> extends HTMLAttributes<T> {
@@ -12,17 +12,37 @@ declare module 'react' {
     }
 }
 
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: Melbourne;
+    src: url('/static/fonts/melbourne-bold.ttf');
+    font-weight: 700;
+  }
+  @font-face {
+      font-family: Melbourne;
+      src: url('/static/fonts/melbourne.ttf');
+      font-weight: 400;
+  }
+  @font-face {
+      font-family: Melbourne;
+      src: url('/static/fonts/melbourne-light.ttf');
+      font-weight: 300;
+  }
+`;
 class MyApp extends App {
     public render() {
         const { Component, pageProps } = this.props;
 
         return (
-            <MosTheme>
-                <Head />
-                <Page>
-                    <Component {...pageProps} />
-                </Page>
-            </MosTheme>
+            <>
+                <GlobalStyle />
+                <MosTheme>
+                    <Head />
+                    <Page>
+                        <Component {...pageProps} />
+                    </Page>
+                </MosTheme>
+            </>
         );
     }
 }
--- packages/components/docs/pages/_document.tsx
+++ packages/components/docs/pages/_document.tsx
@@ -1,24 +1,7 @@
 import Document from 'next/document';
-import { ServerStyleSheet, createGlobalStyle } from 'styled-components';
+import { ServerStyleSheet } from 'styled-components';
 import { ComponentType } from 'react';
 
-const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: Melbourne;
-    src: url('/static/fonts/melbourne-bold.ttf');
-    font-weight: 700;
-  }
-  @font-face {
-      font-family: Melbourne;
-      src: url('/static/fonts/melbourne.ttf');
-      font-weight: 400;
-  }
-  @font-face {
-      font-family: Melbourne;
-      src: url('/static/fonts/melbourne-light.ttf');
-      font-weight: 300;
-  }
-`;
 export default class AppDocument extends Document {
     static async getInitialProps(ctx: any) {
         const sheet = new ServerStyleSheet();
@@ -36,7 +19,6 @@ export default class AppDocument extends Document {
                 ...initialProps,
                 styles: (
                     <>
-                        <GlobalStyle />
                         {initialProps.styles}
                         {sheet.getStyleElement()}
                     </>
