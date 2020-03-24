5322cdb807793f3fb6a33fd521364688409846c9
Wout Sluiter <woutsluiter@gmail.com>
2020-03-24 14:47:19 +0100

    Fix colors imports again

--- packages/components/src/components/Counter/test.tsx
+++ packages/components/src/components/Counter/test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Counter from '.';
 import { mountWithTheme } from '../../utility/styled/testing';
 import 'jest-styled-components';
-import { colors } from '../../themes/MosTheme/colors';
+import colors from '../../themes/MosTheme/colors';
 
 describe('Counter', () => {
     it('should render the variety of severities and use error when no severity is chosen', () => {
