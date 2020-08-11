f5dbd0934bd519fa8715b3f6178b05e239b87c9e
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2020-08-11 14:29:19 +0200

    fix: Added layoutEffect to check empty state and added test

--- packages/components/src/components/Select/SelectList.tsx
+++ packages/components/src/components/Select/SelectList.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useContext } from 'react';
+import React, { FC, useContext, useState, useLayoutEffect } from 'react';
 import ScrollBox from '../ScrollBox';
 import Box from '../Box';
 import Text from '../Text';
@@ -15,13 +15,18 @@ const SelectList: FC<{
     emptyText: string;
     'data-testid'?: string;
 }> = props => {
-    const { options } = useContext(SelectContext);
+    const { options, filter } = useContext(SelectContext);
+    const [isEmpty, setEmpty] = useState(options.length === 0);
+
+    useLayoutEffect(() => {
+        setEmpty(options.length === 0);
+    }, [options, filter]);
 
     return (
         <StyledList data-testid={props['data-testid'] ? `${props['data-testid']}-list}` : undefined}>
             <ScrollBox autoHideScrollBar={false} showInsetShadow={false}>
                 <div style={{ overflow: 'hidden', display: props.isOpen ? 'block' : 'none' }}>
-                    {options.length === 0 && (
+                    {isEmpty && (
                         <Box padding={[12, 18]}>
                             <Text data-testid={props['data-testid'] ? `${props['data-testid']}-list-empty` : undefined}>
                                 {props.emptyText}
@@ -29,10 +34,10 @@ const SelectList: FC<{
                         </Box>
                     )}
                     {/*
-                      * Make sure the children are always rendered, since their lifecycle will update
-                      * the option refs from the context. The children themselves will decide whether
-                      * to return `null` or not based on the filter.
-                      */}
+                     * Make sure the children are always rendered, since their lifecycle will update
+                     * the option refs from the context. The children themselves will decide whether
+                     * to return `null` or not based on the filter.
+                     */}
                     {props.children}
                 </div>
             </ScrollBox>
