0527e642592dab6b6104cd11946aa79cae077278
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2020-09-07 16:34:30 +0200

    chore: Refactored TextField to hooks

--- packages/components/src/components/TextField/story.tsx
+++ packages/components/src/components/TextField/story.tsx
@@ -1,96 +1,36 @@
-import React, { useState, FC } from 'react';
-import { select, text, boolean, number } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react';
+import React, { ComponentProps, useState, useEffect } from 'react';
 import TextField from '.';
-import CurrencyField from '../CurrencyField';
-import NumberField from '../NumberField';
 
-type PropsType = {
-    withClearButton?: boolean;
-    withFeedback?: boolean;
-    isNumber?: boolean;
-    isCurrency?: boolean;
+export default {
+    title: 'TextField',
+    component: TextField,
+    parameters: {
+        actions: {
+            argTypesRegex: '^on.*',
+        },
+    },
 };
 
-const Demo: FC<PropsType> = (props): JSX.Element => {
-    const [stringValue, setStringValue] = useState('');
-    const [numberValue, setNumberValue] = useState(19.12);
-    const locale = select('locale', ['nl-NL', 'en-GB', 'de-DE'], 'nl-NL');
+export const Default = (args: ComponentProps<typeof TextField>) => {
+    const [value, setValue] = useState(args.value || '');
 
-    const sharedProps = {
-        prefix: text('Prefix', 'Username'),
-        suffix: !props.withClearButton ? text('Suffix', '$') : undefined,
-        placeholder: text('Placeholder', 'This is a placeholder'),
-        name: 'fieldname',
-        disabled: boolean('disabled', false),
-        onClear: props.withClearButton
-            ? () => {
-                  setStringValue('');
-              }
-            : undefined,
-        feedback: props.withFeedback
-            ? {
-                  message: text('feedback message', 'This is a feedback message'),
-                  severity: select('feedback severity', ['error', 'success', 'info', 'warning'], 'error'),
-              }
-            : undefined,
-    };
+    useEffect(() => {
+        setValue(args.value);
+    }, [args.value]);
 
-    const numberProps = {
-        value: numberValue,
-        onChange: setNumberValue,
-        disableNegative: boolean('disable negative numbers', false),
-        allowDecimals: boolean('allowDecimals', false),
-        minimumFractionDigits: number('minimumFractionDigits', 0),
-        maximumFractionDigits: number('maximumFractionDigits', 2),
-        characterLimit: number('characterLimit', 10),
-        maximumValue: number('maximumValue', 1000000),
-        locale,
-    };
-
-    const currencyProps = {
-        value: numberValue,
-        onChange: setNumberValue,
-        disableNegative: boolean('disable negative numbers', false),
-        allowDecimals: boolean('allowDecimals', false),
-        characterLimit: number('characterLimit', 10),
-        maximumValue: number('maximumValue', 1000000),
-        locale,
-    };
-
-    const textProps = {
-        value: stringValue,
-        onChange: setStringValue,
-    };
-
-    if (props.isCurrency) {
-        return (
-            <CurrencyField
-                {...sharedProps}
-                {...currencyProps}
-                currency={select('currency', ['USD', 'EUR', 'JPY', 'GBP', 'AUD'], 'EUR')}
-                minor={boolean('minor', false)}
-                feedback={{
-                    severity: 'info',
-                    message: `${numberValue}`,
-                }}
-            />
-        );
-    }
-
-    if (props.isNumber) {
-        return <NumberField {...sharedProps} {...numberProps} />;
-    }
-
-    return <TextField {...sharedProps} {...textProps} />;
+    return (
+        <TextField
+            {...args}
+            value={value}
+            onChange={(value, event) => {
+                args.onChange(value, event);
+                setValue(value);
+            }}
+            onClear={() => {
+                args.onClear?.();
+                setValue('');
+            }}
+            feedback={value === '' ? { message: 'This field cannot be empty', severity: 'error' } : undefined}
+        />
+    );
 };
-
-storiesOf('TextField', module).add('Default', () => <Demo />);
-
-storiesOf('TextField', module).add('With clear button', () => <Demo withClearButton />);
-
-storiesOf('TextField', module).add('With Feedback', () => <Demo withFeedback />);
-
-storiesOf('TextField', module).add('With Number formatting', () => <Demo isNumber />);
-
-storiesOf('TextField', module).add('With Currency formatting', () => <Demo isCurrency />);
