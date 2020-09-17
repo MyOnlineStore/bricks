de6d23a74eeac788a611f6730dd71492710face1
Wout Sluiter <woutsluiter@gmail.com>
2020-09-17 15:32:55 +0200

    fixup! fix(Card): Added testid prop

--- packages/components/src/components/Card/index.tsx
+++ packages/components/src/components/Card/index.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { StyledCard } from './style';
 
 const Card: FC<{
-    'data-testid': string;
+    'data-testid'?: string;
 }> = props => {
     return <StyledCard data-testid={props['data-testid']}>{props.children}</StyledCard>;
 };
