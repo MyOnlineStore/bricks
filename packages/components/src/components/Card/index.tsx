f9adbe8b7b36e4df72b77387375a2f2ec0a32067
Wout <woutsluiter@users.noreply.github.com>
2020-10-07 13:01:37 +0200

    fix(Card): Added testid prop (#601)

--- packages/components/src/components/Card/index.tsx
+++ packages/components/src/components/Card/index.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react';
 import { StyledCard } from './style';
 
-const Card: FC = props => {
-    return <StyledCard>{props.children}</StyledCard>;
+const Card: FC<{
+    'data-testid'?: string;
+}> = props => {
+    return <StyledCard data-testid={props['data-testid']}>{props.children}</StyledCard>;
 };
 
 export default Card;
