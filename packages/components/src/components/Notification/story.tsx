2aa39231203fc3892b00f63d8d41c4dcdc503497
Wout Sluiter <woutsluiter@gmail.com>
2020-11-19 13:47:03 +0100

    feat(Notification): Allow react nodes as message via children

--- packages/components/src/components/Notification/story.tsx
+++ packages/components/src/components/Notification/story.tsx
@@ -2,6 +2,7 @@ import { storiesOf } from '@storybook/react';
 import React from 'react';
 import Notification from '.';
 import { MehIcon } from '@myonlinestore/bricks-assets';
+import Link from '../Link';
 
 storiesOf('Notification', module)
     .add('Success', () => <Notification severity="success" message="Wow, Great job!" />)
@@ -10,4 +11,9 @@ storiesOf('Notification', module)
     .add('Info', () => <Notification severity="info" message="Oops, something wen't wrong. Please try again." />)
     .add('With a custom icon', () => (
         <Notification severity="warning" message="Meh, I am not impressed." icon={<MehIcon />} />
+    ))
+    .add('With react node children', () => (
+        <Notification severity="info">
+            A message with React nodes like a <Link title="Link" target="_blank" href="https://google.com" />
+        </Notification>
     ));
