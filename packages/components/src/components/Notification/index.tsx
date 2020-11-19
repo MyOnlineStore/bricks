73fb52c8850210e987d1b3f777683ad254751aa8
Wout <woutsluiter@users.noreply.github.com>
2020-11-19 14:37:37 +0100

    feat(Notification): Allow react nodes as message via children (#606)

--- packages/components/src/components/Notification/index.tsx
+++ packages/components/src/components/Notification/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, ReactNode } from 'react';
+import React, { Children, FunctionComponent, ReactNode } from 'react';
 import trbl from '../../utility/trbl';
 import Icon from '../Icon';
 import Box from '../Box';
@@ -7,20 +7,21 @@ import { SeverityIcons } from '../../types/SeverityType';
 
 type PropsType = {
     severity: keyof typeof SeverityIcons;
-    message: string;
+    message?: string;
     icon?: ReactNode;
+    'data-testid'?: string;
 };
 
 const Notification: FunctionComponent<PropsType> = (props): JSX.Element => {
     const icon = props.icon !== undefined ? props.icon : SeverityIcons[props.severity];
 
     return (
-        <StyledNotification severity={props.severity}>
+        <StyledNotification severity={props.severity} data-testid={props['data-testid']}>
             <Box padding={trbl(12, 18)} alignItems={'flex-start'}>
                 <Box margin={trbl(3, 6, 0, 0)}>
                     <Icon size="medium" icon={icon} />
                 </Box>
-                {props.message}
+                <Box inline>{(Children.count(props.children) > 0 && props.children) || props.message}</Box>
             </Box>
         </StyledNotification>
     );
---
+++ packages/components/src/components/Notification/test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Notification from '.';
+import { mountWithTheme } from '../../utility/styled/testing';
+
+describe('Notification', () => {
+    it('should be testable with a test-id', () => {
+        const component = mountWithTheme(<Notification severity="info" data-testid="foo" />);
+
+        expect(component.findByTestId('foo')).toHaveLength(1);
+    });
+
+    it('should render a message when no children are present', () => {
+        const component = mountWithTheme(<Notification severity="info" message="A message via prop" />);
+
+        expect(component.text()).toBe('A message via prop');
+    });
+
+    it('should render children instead of the message prop', () => {
+        const component = mountWithTheme(
+            <Notification severity="info" message="No message">
+                But children
+            </Notification>,
+        );
+
+        expect(component.text()).toBe('But children');
+    });
+});
