88b31da813a32c51abf1564eeae2beac1ccb920d
Johan van Tongeren <johan@mijnwebwinkel.nl>
2021-04-29 16:49:03 +0200

    chore: Refactor story to CSF

--- packages/components/src/components/FileInput/story.tsx
+++ packages/components/src/components/FileInput/story.tsx
@@ -1,36 +1,42 @@
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-import Component from './index';
+import React, { ComponentProps } from 'react';
+import FileInput from './index';
 
-const placeholder = (
-    <>
-        Drag and drop here
-        <br />
-        or <u>browse</u>
-    </>
-);
-const dropPlaceholder = <>Drop your file here</>;
+export default {
+    title: 'FileInput',
+    component: FileInput,
+    args: {
+        name: 'HelloWorld',
+        placeholder: (
+            <>
+                Drag and drop here
+                <br />
+                or <u>browse</u>
+            </>
+        ),
+        dropPlaceholder: <>Drop your file here</>,
+    },
+};
 
-storiesOf('FileInput', module)
-    .add('Default', () => {
-        return <Component name="HelloWorld" placeholder={placeholder} dropPlaceholder={dropPlaceholder} />;
-    })
-    .add('Disabled', () => {
-        return <Component name="HelloWorld" placeholder={placeholder} dropPlaceholder={dropPlaceholder} disabled />;
-    })
-    .add('Only PNG/JPG/GIF', () => {
-        return <Component name="HelloWorld" placeholder={placeholder} dropPlaceholder={dropPlaceholder} checkImage />;
-    })
-    .add('Error', () => {
-        return (
-            <Component
-                name="HelloWorld"
-                placeholder={placeholder}
-                dropPlaceholder={dropPlaceholder}
-                feedback={{
-                    message: 'Het bestand Photo.JPG is te groot. Je afbeelding mag maximaal 1 GB zijn',
-                    severity: 'error',
-                }}
-            />
-        );
-    });
+export const Default = (props: ComponentProps<typeof FileInput>) => {
+    return <FileInput {...props} />;
+};
+
+export const Disabled = (props: ComponentProps<typeof FileInput>) => {
+    return <FileInput {...props} disabled />;
+};
+
+export const OnlyImages = (props: ComponentProps<typeof FileInput>) => {
+    return <FileInput {...props} checkImage />;
+};
+
+export const Error = (props: ComponentProps<typeof FileInput>) => {
+    return (
+        <FileInput
+            {...props}
+            feedback={{
+                message: 'Het bestand Photo.JPG is te groot. Je afbeelding mag maximaal 1 GB zijn',
+                severity: 'error',
+            }}
+        />
+    );
+};
