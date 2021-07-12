0f9969b8fbd2152c16ec310b2a3f156e69cb89ed
Johan van Tongeren <johan@mijnwebwinkel.nl>
2021-07-12 11:48:59 +0200

    fix: Fixed z-indexing issues and added a close callback to window

--- packages/components/src/components/ColorField/story.tsx
+++ packages/components/src/components/ColorField/story.tsx
@@ -16,33 +16,56 @@ const validHexcode = (hexcode: string) => {
 
 export const Default = (args: ComponentProps<typeof ColorField>) => {
     const [value, setValue] = useState('#6bde78');
+    const [value2, setValue2] = useState('#e3b00b');
+    const [value3, setValue3] = useState('#ed2157');
     const [hasHexError, setHasHexError] = useState(!validHexcode(value));
 
     return (
-        <ColorField
-            {...args}
-            emptyIsTransparent
-            value={value}
-            initialValue="#6bde78"
-            feedback={
-                hasHexError
-                    ? {
-                          severity: 'error',
-                          message: 'That is not a valid hex code!',
-                      }
-                    : undefined
-            }
-            onBlur={() => {
-                setHasHexError(!validHexcode(value) && value !== 'transparent');
-            }}
-            onChange={(val: string) => {
-                if (validHexcode(val)) {
-                    setHasHexError(false);
+        <>
+            <ColorField
+                {...args}
+                emptyIsTransparent
+                value={value}
+                initialValue="#6bde78"
+                feedback={
+                    hasHexError
+                        ? {
+                              severity: 'error',
+                              message: 'That is not a valid hex code!',
+                          }
+                        : undefined
                 }
+                onBlur={() => {
+                    setHasHexError(!validHexcode(value) && value !== 'transparent');
+                }}
+                onChange={(val: string) => {
+                    if (validHexcode(val)) {
+                        setHasHexError(false);
+                    }
 
-                setValue(val);
-            }}
-        />
+                    setValue(val);
+                }}
+            />
+            <ColorField
+                {...args}
+                emptyIsTransparent
+                value={value2}
+                initialValue="#e3b00b"
+                onChange={(val: string) => {
+                    setValue2(val);
+                }}
+                $margin={[24, 0]}
+            />
+            <ColorField
+                {...args}
+                emptyIsTransparent
+                value={value3}
+                initialValue="#ed2157"
+                onChange={(val: string) => {
+                    setValue3(val);
+                }}
+            />
+        </>
     );
 };
 
